831f6a7f0d2a73db16d0abd39f073f7682ebc879
wusendong <wusendong@gmail.com>
2019-05-30 15:53:57 +0800

    support print to stdout

--- main.go
+++ main.go
@@ -4,51 +4,236 @@ import (
 	"bufio"
 	"bytes"
 	"container/list"
-	"errors"
 	"flag"
 	"fmt"
-	"go/ast"
 	"go/format"
-	"go/parser"
-	"go/printer"
-	"go/token"
 	"io"
 	"io/ioutil"
-	"log"
 	"os"
 	"os/exec"
 	"os/user"
 	"runtime"
-	"sort"
 	"strings"
 )
 
-var rootPkg = flag.String("local", "", "local package name")
+var localPkg = flag.String("local", "", "local package name")
+var wflag = flag.Bool("w", false, "write result to (source) file instead of stdout")
 var vflag = flag.Bool("version", false, "print version")
 
-const version = "gogimport v0.0.1"
+const version = "gogimport v0.0.2"
 
 var stdPkgs = map[string]bool{}
 
 func main() {
+	if err := fmtMain(); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(2)
+	}
+}
+
+func fmtMain() error {
+	var (
+		err error
+		out []byte
+	)
+
 	flag.Parse()
 	if *vflag {
 		fmt.Println(version)
 	}
-	if len(*rootPkg) <= 0 {
+	if len(*localPkg) <= 0 {
 		flag.Usage()
-		log.Fatalln("local must set")
+		return fmt.Errorf("local must set")
 	}
 
-	err := initStdPkg()
+	err = initStdPkg()
 	if err != nil {
-		log.Fatalf("init std package failed: %v", err)
+		return fmt.Errorf("init std package failed: %v", err)
 	}
 
-	files := os.Args[3:]
-	for _, filename := range files {
-		sortFile(filename)
+	files := flag.Args()
+	if len(files) <= 0 {
+		out, err = formatFromStdin()
+		if err != nil {
+			return fmt.Errorf("format failed: %v", err)
+		}
+		fmt.Printf("%s\n", out)
+		return nil
 	}
+	for _, filename := range files {
+		out, err = formatFile(filename)
+		if err != nil {
+			return fmt.Errorf("format failed: %v", err)
+		}
+		if *wflag {
+			if err = ioutil.WriteFile(filename, out, 0); err != nil {
+				return fmt.Errorf("rewrite file failed: %v", err)
+			}
+		} else {
+			fmt.Printf("%s\n", out)
+			return nil
+		}
+	}
+	return nil
+}
+
+var importOnePrefix = []byte("import")
+var importMutiPrefix = []byte("import (")
+var importEnd = []byte{')'}
+
+func formatFromStdin() ([]byte, error) {
+	return formatFromIO(os.Stdin)
+}
+
+func formatFile(filename string) ([]byte, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	return formatFromIO(file)
+}
+
+func formatFromIO(rd io.Reader) ([]byte, error) {
+	lines, err := scannFileLines(rd)
+	if err != nil {
+		return nil, err
+	}
+	formatLines(lines)
+	buf := linesToBuffer(lines)
+	return buf.Bytes(), nil
+	return format.Source(buf.Bytes())
+}
+
+func scannFileLines(file io.Reader) (*list.List, error) {
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+	preproccess, err := format.Source(content)
+	if err != nil {
+		return nil, err
+	}
+
+	rd := bufio.NewScanner(bytes.NewBuffer(preproccess))
+	l := list.New()
+	for rd.Scan() {
+		l.PushBack(rd.Bytes())
+	}
+	return l, nil
+}
+
+func formatLines(lines *list.List) {
+	var next = lines.Front()
+	for {
+		next = findImportLine(next)
+		if next == nil {
+			break
+		}
+		next = sortImport(lines, next.Next(), *localPkg)
+		if next == nil {
+			break
+		}
+	}
+
+}
+
+func findImportLine(head *list.Element) *list.Element {
+	for head != nil && !bytes.HasPrefix(head.Value.([]byte), importMutiPrefix) {
+		head = head.Next()
+	}
+	return head
+}
+
+var newline = []byte{}
+
+func sortImport(lines *list.List, head *list.Element, localPrefix string) *list.Element {
+	// insert placeholder
+	std := lines.InsertBefore(newline, head)
+	thirdparty := lines.InsertBefore(newline, head)
+	local := lines.InsertBefore(newline, head)
+
+	// sort imports
+	var hasStd, hasLocal, hasThirdParty bool
+	var cur, next = head, head.Next()
+	for ; cur != nil && !bytes.HasPrefix(cur.Value.([]byte), importEnd); cur = next {
+		// fmt.Printf("\n***********\n%s\n********\n\n", linesToBuffer(lines))
+		next = cur.Next()
+		pkg := getImportPkg(cur.Value.([]byte))
+		if pkg == "" {
+			// fmt.Printf("#!%s\n", cur.Value)
+			lines.Remove(cur)
+			continue
+		}
+		switch {
+		case strings.HasPrefix(pkg, localPrefix):
+			hasLocal = true
+			insertSort(lines, local, cur)
+		case stdPkgs[pkg]:
+			hasStd = true
+			insertSort(lines, std, cur)
+			// fmt.Printf("#$%s", cur.Value)
+			// fmt.Printf("#&%s", cur.Value)
+		default:
+			hasThirdParty = true
+			insertSort(lines, thirdparty, cur)
+			// fmt.Printf("#@%s", cur.Value)
+		}
+		// fmt.Printf("\t pkg: %s\n", pkg)
+	}
+
+	// remove placeholder that does'n needed
+	lines.Remove(local)
+	if (!hasLocal && hasThirdParty) || !hasThirdParty {
+		lines.Remove(local)
+	}
+	if (!hasThirdParty && !hasLocal && hasStd) || !hasStd {
+		lines.Remove(std)
+	}
+
+	return cur
+}
+
+func insertSort(lines *list.List, head *list.Element, e *list.Element) {
+	for ; head != nil; head = head.Prev() {
+		if bytes.Compare(head.Prev().Value.([]byte), importMutiPrefix) != 0 {
+			break
+		}
+		if len(head.Prev().Value.([]byte)) > 0 {
+			break
+		}
+		if importLessThan(e.Value.([]byte), head.Value.([]byte)) {
+			break
+		}
+	}
+	lines.MoveBefore(e, head)
+}
+
+func importLessThan(a, b []byte) bool {
+	return getImportPkg(a) < getImportPkg(b)
+}
+
+func getImportPkg(a []byte) string {
+	index := bytes.Index(a, []byte{'"'})
+	if index > 0 {
+		return string(bytes.Trim(bytes.TrimSpace(a[index:]), `"`))
+	}
+	return string(bytes.Trim(bytes.TrimSpace(a), `"`))
+}
+
+func linesToBuffer(lines *list.List) *bytes.Buffer {
+	head := lines.Front()
+	buf := bytes.Buffer{}
+	for head != nil {
+		buf.Write(head.Value.([]byte))
+		head = head.Next()
+		if head == nil {
+			break
+		}
+		buf.Write([]byte{'\n'})
+	}
+	return &buf
 }
 
 func initStdPkg() error {
@@ -105,147 +290,4 @@ func initStdPkg() error {
 		stdPkgs[sc.Text()] = true
 	}
 	return nil
-}
-
-// func (st *Sorter) init(file *os.File) error {
-// 	var err error
-
-// 	src, err := ioutil.ReadAll(file)
-// 	if err != nil {
-// 		return errors.New("read file " + st.filename + " error: " + err.Error())
-// 	}
-// 	src = append(src, []byte("\n\n\n")...)
-
-// 	st.lines, err = getLines(src)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	parserMode := parser.ParseComments
-
-// 	st.fset = token.NewFileSet()
-// 	st.f, err = parser.ParseFile(st.fset, "", src, parserMode)
-// 	if err != nil {
-// 		log.Printf("parse file error :%s", err.Error())
-// 		return err
-// 	}
-// 	return nil
-// }
-
-// func (st *Sorter) Write(file *os.File) error {
-// 	var buf = &bytes.Buffer{}
-// 	if err := printer.Fprint(buf, st.fset, st.f); err != nil {
-// 		return err
-// 	}
-// 	out, err := format.Source(buf.Bytes())
-// 	if err != nil {
-// 		return err
-// 	}
-// 	_, err = file.Seek(0, 0)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	err = file.Truncate(int64(buf.Len()))
-// 	if err != nil {
-// 		return err
-// 	}
-// 	_, err = file.Write(out)
-// 	return err
-// }
-
-var importOnePrefix = []byte("import")
-var importMutiPrefix = []byte("import (")
-var importEnd = []byte{')'}
-
-func sortFile(filename string) ([]byte, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	lines := scannFileLines(file)
-	dealLines(lines)
-	buf := linesToBuffer(lines)
-
-	return format.Source(buf.Bytes())
-}
-
-func sortFromIOl()
-
-func scannFileLines(file *os.File) *list.List {
-	rd := bufio.NewScanner(file)
-	l := list.New()
-	for rd.Scan() {
-		l.PushBack(rd.Bytes())
-	}
-	return l
-}
-
-func dealLines(lines *list.List) {
-	var next = lines.Front()
-	for {
-		next = findImportLine(lines.Front())
-		if next == nil {
-			break
-		}
-		next = sortImport(lines, next.Next(), "")
-		if next == nil {
-			break
-		}
-	}
-
-}
-
-func findImportLine(head *list.Element) *list.Element {
-	for head != nil && !bytes.HasPrefix(head.Value.([]byte), importMutiPrefix) {
-		head = head.Next()
-	}
-	return head
-}
-
-var newline = []byte{'\n'}
-
-func sortImport(lines *list.List, head *list.Element, localPrefix string) *list.Element {
-	std := lines.InsertBefore(newline, head)
-	local := lines.InsertBefore(newline, head)
-	thirdparty := lines.InsertBefore(newline, head)
-
-	var next *list.Element
-	for ; head != nil && !bytes.HasPrefix(head.Value.([]byte), importEnd); head = next {
-		next = head.Next()
-		pkg := getImportPkg(head.Value.([]byte))
-		switch {
-		case stdPkgs[pkg]:
-			lines.MoveBefore(head, std)
-		case strings.HasPrefix(pkg, localPrefix):
-			lines.MoveBefore(head, local)
-		default:
-			lines.MoveBefore(head, thirdparty)
-		}
-	}
-	return head
-
-}
-
-func getImportPkg(a []byte) string {
-	index := bytes.Index(a, []byte{'"'})
-	if index > 0 {
-		return string(bytes.Trim(bytes.TrimSpace(a[index:]), `"`))
-	}
-	return string(bytes.Trim(bytes.TrimSpace(a), `"`))
-}
-
-func linesToBuffer(lines *list.List) *bytes.Buffer {
-	head := lines.Front()
-	buf := bytes.Buffer{}
-	for head != nil {
-		buf.Write(head.Value.([]byte))
-		head = head.Next()
-		if head == nil {
-			break
-		}
-		buf.Write([]byte{'\n'})
-	}
-	return &buf
 }
